5cfedd6e10cccafe5cdb79c62677fdf3ffdefd24
Danilo Barros <danilobjr@gmail.com>
2026-06-12 17:26:16 -0300

    chore(root.config): remove todo

--- src/config/root.config.tsx
+++ src/config/root.config.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 
 import { RouterConfig } from './router.config'
 
-// TODO providers and friends on this file
-
 export function RootConfig() {
   return (
     <StrictMode>
