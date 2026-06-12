9a80841666e783c0e4ff3dcfc9a483a0a67cfcd6
Danilo Barros <danilobjr@gmail.com>
2026-06-12 17:57:07 -0300

    feat(router): configure RMA hash routes

--- src/config/router.config.tsx
+++ src/config/router.config.tsx
@@ -1,9 +1,70 @@
-import { HelloPage } from '@/pages/hello.page'
+import {
+  createHashHistory,
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from '@tanstack/react-router'
 
-// TODO discard HelloPage
-// TODO setup react-router v7 hash routes
-// TODO import page components here
+import { RmaCreateContainer } from '@/pages/rma/create/rma-create.container'
+import { RmaListContainer } from '@/pages/rma/list/rma-list.container'
+import { RmaUpdateContainer } from '@/pages/rma/update/rma-update.container'
+import { Layout } from '@/components/app/layout/layout.app'
+
+const rootRoute = createRootRoute({
+  component: function RootRoute() {
+    return (
+      <Layout>
+        <Outlet />
+      </Layout>
+    )
+  },
+})
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: function IndexRoute() {
+    return <Navigate to="/rma" />
+  },
+})
+
+const rmaListRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/rma',
+  component: RmaListContainer,
+})
+
+const rmaCreateRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/rma/create',
+  component: RmaCreateContainer,
+})
+
+const rmaUpdateRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/rma/$rmaId',
+  component: function RmaUpdateRoute() {
+    const { rmaId } = rmaUpdateRoute.useParams()
+
+    return <RmaUpdateContainer rmaId={rmaId} />
+  },
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, rmaListRoute, rmaCreateRoute, rmaUpdateRoute])
+
+const hashHistory = createHashHistory()
+
+const router = createRouter({ routeTree, history: hashHistory })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 export function RouterConfig() {
-  return <HelloPage />
+  return <RouterProvider router={router} />
 }
--- src/pages/hello.page.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from '@/components/ui/button'
-
-export function HelloPage() {
-  return <Button>I'm a useless example page. You can delete me</Button>
-}
