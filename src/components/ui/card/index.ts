45a24ed7f2be67e7bf6a8e4d4a3bf6bd9217c1ab
Danilo Barros <danilobjr@gmail.com>
2026-06-15 03:53:53 -0300

    refactor(comp/ui/card): apply folder and style conventions

---
+++ src/components/ui/card/index.ts
@@ -0,0 +1,19 @@
+export type {
+  CardActionProps,
+  CardContentProps,
+  CardDescriptionProps,
+  CardFooterProps,
+  CardHeaderProps,
+  CardProps,
+  CardTitleProps,
+} from './card.ui'
+
+export {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './card.ui'
